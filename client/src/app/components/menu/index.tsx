01323e42c7c73db4f87c96bc97663c95a5e02d7b
Kernichnyy Andrey <only.exlight@gmail.com>
2019-10-08 01:20:17 +0700

    Menu component

--- client/src/app/components/menu/index.tsx
+++ client/src/app/components/menu/index.tsx
@@ -1,7 +1,16 @@
 import * as React from 'react';
+import { MENU } from '@app/consts/menu';
 
 export function Menu() {
     return (
-        <div></div>
+        <nav id="menu" className="d-flex align-items-center position-sticky shadow">
+            <div className="container d-flex justify-content-between">
+                <div>Code of Power</div>
+                <div>
+                    {MENU.map(item =>
+                        (<a href={item.route} className="menu-item font-weight-bold mx-2">{item.title}</a>))}
+                </div>
+            </div>
+        </nav>
     );
 }
