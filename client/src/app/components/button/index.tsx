4465f2a7466962078ebac9a50ac77b4fabff3042
Kernichnyy Andrey <only.exlight@gmail.com>
2019-10-08 01:49:40 +0700

    Contact us page layout

--- client/src/app/components/button/index.tsx
+++ client/src/app/components/button/index.tsx
@@ -3,10 +3,13 @@ import * as React from 'react';
 interface IButton {
     type: 'primary' | 'danger' | 'default';
     children: string;
+    cssClass?: string;
 }
 
 export function Button(props: IButton) {
     return (
-        <button className={`cop-button rounded font-weight-bold ${props.type}`}>{props.children}</button>
+        <button className={`cop-button rounded font-weight-bold ${props.type} ${props.cssClass || ''}`}>
+            {props.children}
+        </button>
     );
 }
--- client/src/app/screens/contact-us/index.tsx
+++ client/src/app/screens/contact-us/index.tsx
@@ -1,7 +1,30 @@
 import * as React from 'react';
+import { Button } from '@app/components/button';
 
 export function ContactUs() {
     return (
-        <section className="vh-100"></section>
+        <section className="vh-100">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-3">
+                        <h1 className="text-white">Связаться с нами</h1>
+                        <div className="d-flex flex-column">
+                            <Button cssClass="mb-4" type="primary">Стать клиентом</Button>
+                            <Button type="default">Присоедениться</Button>
+                        </div>
+
+                    </div>
+                    <div className="offset-md-2 col-5">
+                        <span className="text-white">
+                            Заинтересовали наши услуги, хотите присоедениться к нам
+                            или есть другие вопросы или предложения?
+                            Заполните простую форму или отправьте нам письмо на&nbsp;
+                            <a href="mailto:">code-power@gmail.com</a>&nbsp;
+                            и мы обязательно свяжемся с Вами.
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
