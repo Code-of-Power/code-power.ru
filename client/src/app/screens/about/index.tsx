d4370fffebf9eecd0015258cae85eca523b7b70e
Kernichnyy Andrey <only.exlight@gmail.com>
2019-10-08 01:20:28 +0700

    About section

--- client/src/app/screens/about/index.tsx
+++ client/src/app/screens/about/index.tsx
@@ -1,9 +1,35 @@
 import * as React from 'react';
+import { Button } from '@app/components/button';
 
 export function About() {
     return (
-        <section>
-            About
+        <section id="about" className="vh-100">
+            <div className="container">
+                <div className="tagline row justify-content-center">
+                    <div className="col-10">
+                        <h1 className="head">
+                            <span className="text-primary font-weight-bold">Мощь</span>
+                            &nbsp;
+                            <span className="text-white font-weight-bold">нашего кода</span>
+                        </h1>
+                        <h2 className="text-white font-weight-bold">для ваших технологичных продуктов!</h2>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="offset-md-1 col-6">
+                        <h2>
+                            <span className="text-primary font-weight-bold">Code of Power</span>
+                            &nbsp;
+                            <span className="text-white font-weight-bold">
+                                - молодая команда независимых IT-специалистов разных профилей.
+                            </span>
+                        </h2>
+                    </div>
+                    <div className="offset-md-1 col-3 d-flex align-items-center">
+                        <Button type="primary">Узнать больше</Button>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
