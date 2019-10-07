6cfa56497c490cd37d74f86a895d749d321d3856
Kernichnyy Andrey <only.exlight@gmail.com>
2019-10-08 01:17:16 +0700

    Menu component

---
+++ client/src/app/consts/menu.ts
@@ -0,0 +1,18 @@
+import { IMenuItem } from '@app/interfaces/app';
+
+export const MENU: IMenuItem[] = [{
+    title: 'О нас',
+    route: '#about'
+}, {
+    title: 'Команда',
+    route: '#team'
+}, {
+    title: 'Проекты',
+    route: '#projects'
+}, {
+    title: 'Услуги',
+    route: '#services'
+}, {
+    title: 'Контакты',
+    route: '#contacts'
+}];
