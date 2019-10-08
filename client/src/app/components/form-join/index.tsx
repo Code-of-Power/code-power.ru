8b9ac66907083d83cee4477d9a93e089f7210b06
Kernichnyy Andrey <only.exlight@gmail.com>
2019-10-08 21:52:27 +0700

    Join screen

--- client/src/app/components/form-join/index.tsx
+++ client/src/app/components/form-join/index.tsx
@@ -1,10 +1,29 @@
 import * as React from 'react';
+import { Button } from '../button';
 
 export function FormJoin() {
     return (
         <form>
             <div className="form-group">
-                <input type="text" className="form-control" />
+                <input type="text" placeholder="Ваш e-mail" className="form-control" />
+            </div>
+            <div className="form-group">
+                <input type="text" placeholder="Как Вас зовут?" className="form-control" />
+            </div>
+            <div className="form-group">
+                <input type="text" placeholder="Ваша специализация" className="form-control" />
+            </div>
+            <div className="form-group">
+                <textarea name="message" placeholder="Введите сообщение..."
+                    rows={3} className="form-control"></textarea>
+            </div>
+            <div className="d-flex">
+                <div className="w-50 d-flex align-items-center mr-4">
+                    <Button type="danger">Отмена</Button>
+                </div>
+                <div className="w-50">
+                    <Button type="primary">Отправить</Button>
+                </div>
             </div>
         </form>
     )
--- client/src/app/screens/join/index.tsx
+++ client/src/app/screens/join/index.tsx
@@ -1,7 +1,27 @@
 import * as React from 'react';
+import { FormJoin } from '@app/components/form-join';
 
 export function Join() {
     return (
-        <section className="vh-100"></section>
+        <section className="vh-100">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-5">
+                        <div className="text-white h3">
+                            Присоедениться к нам:
+                        </div>
+                        <FormJoin />
+                    </div>
+                    <div className="col-4 offset-md-1">
+                        <div className="h3 text-white">
+                            Мы будем рады новым участникам нашей дружной команды,
+                                готовым развиваться и расти вместе с нами.
+                                Мы молоды и выбираем свой путь развития и совместный рост.
+                                Мы помогаем и поддерживаем друг друга. Готов двигаться с нами?
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
