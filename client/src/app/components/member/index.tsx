90667168ee39ca714110d233ea6a5f7f4e4a5566
Kernichnyy Andrey <only.exlight@gmail.com>
2019-10-10 02:07:43 +0700

    Members

--- client/src/app/components/member/index.tsx
+++ client/src/app/components/member/index.tsx
@@ -1,15 +1,21 @@
 import * as React from 'react';
 import { IMember } from '@app/interfaces/app';
+import { connect } from 'react-redux';
+import { IStore } from '@app/store';
+import { actions } from '@app/reducers/team-screen';
 
 interface IMemberProps {
     member: IMember;
+    [field: string]: any
 }
 
-export function Member(props: IMemberProps) {
+export const Member = connect((state: IStore) => state.teamReducer)((props: IMemberProps) => {
     const { member } = props;
     return (
         <div className="member p-2">
-            <div className="rounded position-relative">
+            <div className="rounded position-relative"
+                onMouseEnter={() => props.dispatch(actions.setActiveMember(member))}
+                onMouseLeave={() => props.dispatch(actions.setActiveMember(null))}>
                 <img src={member.photoUrl} alt={member.name} />
                 <div className="member-description position-absolute w-100">
                     <div className="text-white text-center font-weight-bold">{member.name}</div>
@@ -18,4 +24,4 @@ export function Member(props: IMemberProps) {
             </div>
         </div>
     );
-}
+});
--- client/src/app/screens/peoples/index.tsx
+++ client/src/app/screens/peoples/index.tsx
@@ -1,23 +1,45 @@
 import * as React from 'react';
 import { MEMBERS } from '@app/consts/members';
 import { Member } from '@app/components/member';
+import { connect } from 'react-redux';
+import { IStore } from '@app/store';
+import { IMember } from '@app/interfaces/app';
 
-export function Peoples() {
-    return (
-        <section className="vh-100">
-            <div className="container">
-                <div className="row">
-                    <div className="col-6 d-flex flex-wrap">
-                        {MEMBERS.map(m => (<Member member={m} />))}
-                    </div>
-                    <div className="offset-md-1 col-4">
-                        <div className="h3 text-white">
-                            Наша команда - молодые, активно развивающиеся специлисты IT отрасли,
-                            специализирующиеся на веб-разработке, тестировании и искуственном интелекте.
+interface IDescriptionProps {
+    activeMember: IMember;
+}
+
+function Description(props: IDescriptionProps) {
+    return !props.activeMember ? (
+        <div className="h3 text-white">
+            Наша команда - молодые, активно развивающиеся специлисты IT отрасли,
+            специализирующиеся на веб-разработке, тестировании и искуственном интелекте.
+        </div>
+    ) : (
+            <div className="text-white">
+                {props.activeMember.description}
+            </div>
+        );
+}
+
+interface IPeoplesProps {
+    [field: string]: any;
+}
+
+export const Peoples = connect((state: IStore) => state.teamReducer)(
+    (props: IPeoplesProps) => {
+        return (
+            <section className="vh-100">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6 d-flex flex-wrap">
+                            {MEMBERS.map(m => (<Member member={m} />))}
+                        </div>
+                        <div className="offset-md-1 col-4">
+                            <Description activeMember={props.activeMember} />
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    );
-}
+            </section>
+        );
+    });
