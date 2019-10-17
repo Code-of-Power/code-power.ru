3725e87b2407f46fc110092cc63a72e54da7d0d7
Kernichnyy Andrey <only.exlight@gmail.com>
2019-10-18 00:34:32 +0700

    Update Button component

--- client/src/app/components/button/index.tsx
+++ client/src/app/components/button/index.tsx
@@ -2,14 +2,25 @@ import * as React from 'react';
 
 interface IButton {
     type: 'primary' | 'danger' | 'default';
+    disabled?: boolean;
     children: string;
     cssClass?: string;
+    onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
 export function Button(props: IButton) {
+    const { children, cssClass, onClick, type, disabled } = props;
+    const click = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        if (onClick) {
+            onClick(e);
+        }
+    };
+
     return (
-        <button className={`cop-button rounded font-weight-bold ${props.type} ${props.cssClass || ''}`}>
-            {props.children}
+        <button className={`cop-button rounded font-weight-bold ${type} ${cssClass || ''}`}
+            disabled={disabled}
+            onClick={(e) => click(e)}>
+            {children}
         </button>
     );
 }
