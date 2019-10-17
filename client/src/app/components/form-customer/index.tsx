bd139b7f0d9dd5027058ab77b55ba35915baa968
Kernichnyy Andrey <only.exlight@gmail.com>
2019-10-18 00:35:32 +0700

    Forms: handle input, conetct to store, handle send

--- client/src/app/components/form-customer/index.tsx
+++ client/src/app/components/form-customer/index.tsx
@@ -1,23 +1,64 @@
 import * as React from 'react';
 import { Button } from '../button';
 import { CUSTOMER_OPTIONS } from '@app/consts/options';
+import { connect } from 'react-redux';
+import { IStore } from '@app/store';
+import { sendCustomerMessage } from '@app/api';
+import { IClientFormApi } from '@app/interfaces/api';
+import { CustomerForm } from '@app/models/customer.model';
 
-export function FormCustomer() {
+const mapState = (state: IStore) => ({
+    isSending: state.forms.isSendingCustomer,
+    err: state.forms.errSendCustomer,
+});
+
+const mapProps = ({ sendCustomerMessage });
+
+interface IFormCustomerComponent {
+    isSending: boolean;
+    err: boolean;
+    sendCustomerMessage: (form: IClientFormApi) => Promise<void>
+}
+
+function FormCustomerComponent(props: IFormCustomerComponent) {
+    const { isSending, err, sendCustomerMessage } = props;
+    const [form, setForm] = React.useState<CustomerForm>(new CustomerForm());
+    const send = async () => {
+        try {
+            await form.validate();
+            sendCustomerMessage(form);
+        } catch (error) {
+            console.warn(error);
+        }
+    }
+
     return (
-        <form>
+        <form onSubmit={(e) => e.preventDefault()}>
             <div className="d-flex">
                 <div className="form-group w-50 mr-4">
-                    <input type="text" placeholder="Ваш e-mail" className="form-control" />
+                    <input type="text" placeholder="Ваш e-mail" className="form-control"
+                        value={form.email}
+                        onChange={(e) => setForm(form.nextState({ ...form, email: e.target.value }))}
+                    />
                 </div>
                 <div className="form-group w-50">
-                    <input type="text" placeholder="Компания" className="form-control" />
+                    <input type="text" placeholder="Компания" className="form-control"
+                        value={form.company}
+                        onChange={(e) => setForm(form.nextState({ ...form, company: e.target.value }))}
+                    />
                 </div>
             </div>
             <div className="form-group">
-                <input type="text" placeholder="Как Вас зовут?" className="form-control" />
+                <input type="text" placeholder="Как Вас зовут?" className="form-control"
+                    value={form.name}
+                    onChange={(e) => setForm(form.nextState({ ...form, name: e.target.value }))}
+                />
             </div>
             <div className="form-group">
-                <select className="form-control">
+                <select className="form-control"
+                    value={form.interested}
+                    onChange={(e) => setForm(form.nextState({ ...form, interested: e.target.value }))}
+                >
                     {
                         CUSTOMER_OPTIONS.map(o =>
                             (<option disabled={o.disabled} selected={o.disabled} value={o.value}>
@@ -29,16 +70,23 @@ export function FormCustomer() {
             </div>
             <div className="form-group">
                 <textarea name="message" placeholder="Введите сообщение..."
-                    rows={3} className="form-control"></textarea>
+                    rows={3} className="form-control"
+                    value={form.message}
+                    onChange={(e) => setForm(form.nextState({ ...form, message: e.target.value }))}
+                ></textarea>
             </div>
             <div className="d-flex">
                 <div className="w-50 d-flex align-items-center mr-4">
                     <Button type="danger">Отмена</Button>
                 </div>
                 <div className="w-50">
-                    <Button type="primary">Отправить</Button>
+                    <Button type="primary" disabled={isSending}
+                        onClick={() => send()}
+                    >Отправить</Button>
                 </div>
             </div>
         </form>
     )
 }
+
+export const FormCustomer = connect(mapState, mapProps)(FormCustomerComponent)
--- client/src/app/components/form-join/index.tsx
+++ client/src/app/components/form-join/index.tsx
@@ -1,30 +1,75 @@
 import * as React from 'react';
 import { Button } from '../button';
+import { connect } from 'react-redux';
+import { IStore } from '@app/store';
+import { sendJoinMessage } from '@app/api';
+import { IJoinFormApi } from '@app/interfaces/api';
+import { JoinForm } from '@app/models/join.model';
 
-export function FormJoin() {
+const mapState = (state: IStore) => ({
+    isSending: state.forms.isSendingCustomer,
+    err: state.forms.errSendCustomer,
+});
+
+interface IFormJoinComponent {
+    isSending: boolean;
+    err: boolean;
+    sendJoinMessage: (form: IJoinFormApi) => Promise<void>
+}
+
+const mapProps = ({ sendJoinMessage });
+
+function FormJoinComponent(props: IFormJoinComponent) {
+    const { isSending, err, sendJoinMessage } = props;
+    const [form, setForm] = React.useState<JoinForm>(new JoinForm());
+    const send = async () => {
+        try {
+            await form.validate();
+            sendJoinMessage(form);
+        } catch (error) {
+            console.warn(error);
+        }
+    }
+
     return (
-        <form>
+        <form onSubmit={(e) => e.preventDefault()}>
             <div className="form-group">
-                <input type="text" placeholder="Ваш e-mail" className="form-control" />
+                <input type="text" placeholder="Ваш e-mail" className="form-control"
+                    value={form.email}
+                    onChange={(e) => setForm(form.nextState({ ...form, email: e.target.value }))}
+                />
             </div>
             <div className="form-group">
-                <input type="text" placeholder="Как Вас зовут?" className="form-control" />
+                <input type="text" placeholder="Как Вас зовут?" className="form-control"
+                    value={form.name}
+                    onChange={(e) => setForm(form.nextState({ ...form, name: e.target.value }))}
+                />
             </div>
             <div className="form-group">
-                <input type="text" placeholder="Ваша специализация" className="form-control" />
+                <input type="text" placeholder="Ваша специализация" className="form-control"
+                    value={form.specialisation}
+                    onChange={(e) => setForm(form.nextState({ ...form, specialisation: e.target.value }))}
+                />
             </div>
             <div className="form-group">
                 <textarea name="message" placeholder="Введите сообщение..."
-                    rows={3} className="form-control"></textarea>
+                    rows={3} className="form-control"
+                    value={form.message}
+                    onChange={(e) => setForm(form.nextState({ ...form, message: e.target.value }))}
+                ></textarea>
             </div>
             <div className="d-flex">
                 <div className="w-50 d-flex align-items-center mr-4">
                     <Button type="danger">Отмена</Button>
                 </div>
                 <div className="w-50">
-                    <Button type="primary">Отправить</Button>
+                    <Button type="primary" disabled={isSending}
+                        onClick={() => send()}
+                    >Отправить</Button>
                 </div>
             </div>
         </form>
     )
 }
+
+export const FormJoin = connect(mapState, mapProps)(FormJoinComponent);
