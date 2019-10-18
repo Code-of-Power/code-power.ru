8e5cefff13f6bcc5ebb334dfd16c7ea92d52704b
Kernichnyy Andrey <only.exlight@gmail.com>
2019-10-19 02:38:24 +0700

    React: API functions - add body to query

--- client/src/app/api/index.ts
+++ client/src/app/api/index.ts
@@ -1,21 +1,22 @@
-import { IClientFormApi, IJoinFormApi } from '@app/interfaces/api';
 import { Dispatch } from 'redux';
 import { actions } from '@app/reducers/forms';
 import { DOMAIN } from '@app/consts/env';
+import { JoinForm } from '@app/models/join.model';
+import { CustomerForm } from '@app/models/customer.model';
 
 const OPTIONS: RequestInit = {
     method: 'POST',
     cache: 'no-cache',
     headers: {
         'Content-Type': 'application/json'
-    }
+    },
 };
 
-export function sendJoinMessage(form: IJoinFormApi) {
+export function sendJoinMessage(form: JoinForm) {
     return async (dispatch: Dispatch) => {
         try {
             dispatch(actions.isSendingJoin());
-            const res = await fetch(`${DOMAIN}/api/v1/join`, OPTIONS);
+            const res = await fetch(`${DOMAIN}/api/v1/join`, { ...OPTIONS, body: JSON.stringify(form.toJSON()) });
             dispatch(actions.wasSendJoin());
         } catch (error) {
             dispatch(actions.errSendJoin());
@@ -23,11 +24,11 @@ export function sendJoinMessage(form: IJoinFormApi) {
     };
 }
 
-export function sendCustomerMessage(form: IClientFormApi) {
+export function sendCustomerMessage(form: CustomerForm) {
     return async (dispatch: Dispatch) => {
         try {
             dispatch(actions.isSendingCustomer());
-            const res = await fetch(`${DOMAIN}/api/v1/customer`, OPTIONS);
+            const res = await fetch(`${DOMAIN}/api/v1/customer`, { ...OPTIONS, body: JSON.stringify(form.toJSON()) });
             dispatch(actions.wasSendCustomer());
         } catch (error) {
             dispatch(actions.errSendCustomer());
