8a22956ab8ac3b46ee7eeb4405c9733f3c89ccc8
Kernichnyy Andrey <only.exlight@gmail.com>
2019-10-19 17:24:39 +0700

    Adaptive menu

--- client/src/app/components/menu/index.tsx
+++ client/src/app/components/menu/index.tsx
@@ -6,9 +6,12 @@ export function Menu() {
         <nav id="menu" className="d-flex align-items-center position-absolute shadow">
             <div className="container d-flex justify-content-between">
                 <div>Code of Power</div>
-                <div>
+                <div className="d-none d-md-block">
                     {MENU.map((item, i) =>
                         (<a key={i} href={item.route} className="menu-item font-weight-bold mx-2">{item.title}</a>))}
+                </div>
+                <div className="d-flex align-items-center bar">
+                    <i className="la la-navicon"></i>
                 </div>
             </div>
         </nav>
