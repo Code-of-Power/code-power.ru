7eeba97df09dc304de829ce8c1e637ff880c43a3
Kernichnyy Andrey <only.exlight@gmail.com>
2019-10-22 23:54:04 +0700

    Adaptation sceen contact-us

--- client/src/app/screens/contact-us/index.tsx
+++ client/src/app/screens/contact-us/index.tsx
@@ -12,21 +12,22 @@ interface IContactUsProps {
 function ContactUs(props: IContactUsProps) {
     const { dispatch } = props;
     return (
-        <section className="vh-100">
+        <section className="vh-100" id="contact-us">
             <div className="container">
                 <div className="row justify-content-center align-items-center">
                     <div className="col-sm-12 col-md-3 ">
-                        <h1 className="text-white h3">Связаться с нами:</h1>
+                        <h1 className="text-white h3 contact-head">Связаться с нами:</h1>
                         <div className="d-flex flex-column">
-                            <Button cssClass="mb-4" type="primary"
+                            <Button cssClass="mb-2" type="primary"
                                 onClick={() => dispatch(actions.setScrenState(E_CONTACT_SCREEN_STATE.CUSTOMER))}
                             >Стать клиентом</Button>
+                            <div className="text-white text-center font-weight-bold mb-2">или</div>
                             <Button type="default"
                                 onClick={() => dispatch(actions.setScrenState(E_CONTACT_SCREEN_STATE.JOIN))}
                             >Присоедениться</Button>
                         </div>
                     </div>
-                    <div className="col-sm-12 offset-md-2 col-md-5">
+                    <div className="col-sm-12 offset-md-2 col-md-5 description">
                         <span className="text-white h3">
                             Заинтересовали наши услуги? Хотите присоедениться к нам,
                             есть другие вопросы или предложения?
